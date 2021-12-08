bac1224ce4c7ebc56b9dc4d498ba45956e9e1fef
xan <xan.qin@madgaze.com>
2021-12-08 12:28:17 +0800

    双向链表简单实现与封装

--- Assets/AssetBundleBusinessFramework/Scripts/Manager/ResourceManager.cs
+++ Assets/AssetBundleBusinessFramework/Scripts/Manager/ResourceManager.cs
@@ -1,5 +1,5 @@
 ﻿using AssetBundleBusinessFramework.Tools;
-
+using System.Collections.Generic;
 
 namespace AssetBundleBusinessFramework {
 
@@ -192,5 +192,113 @@ namespace AssetBundleBusinessFramework {
 				Tail = Head;
             }
 		}
+
 	}
+
+	public class CMapList<T> where T : class, new() {
+		DoubleLinkedList<T> m_DLink = new DoubleLinkedList<T>();
+		Dictionary<T, DoubleLinkedListNode<T>> m_FindMap = new Dictionary<T, DoubleLinkedListNode<T>>();
+
+		// 析构函数
+		~CMapList() {
+			Clear();
+		}
+
+		/// <summary>
+		/// 情况列表
+		/// </summary>
+		public void Clear() {
+            while (m_DLink.Tail!=null)
+            {
+				Remove(m_DLink.Tail.Current);
+            }
+		}
+
+		/// <summary>
+		/// 插入一个节点到表头
+		/// </summary>
+		/// <param name="t"></param>
+		public void InsertToHead(T t) {
+			DoubleLinkedListNode<T> node = null;
+            if (m_FindMap.TryGetValue(t,out node) && node !=null)
+            {
+				m_DLink.AddToHeader(node);
+				return;
+            }
+
+			m_DLink.AddToHeader(t);
+			m_FindMap.Add(t,m_DLink.Head);
+		}
+
+		/// <summary>
+		/// 从表尾弹出一个节点
+		/// </summary>
+		public void Pop() {
+            if (m_DLink.Tail !=null)
+            {
+				Remove(m_DLink.Tail.Current);
+            }
+		}
+
+		/// <summary>
+		/// 删除某个节点
+		/// </summary>
+		/// <param name="t"></param>
+		public void Remove(T t) {
+			DoubleLinkedListNode<T> node = null;
+            if (m_FindMap.TryGetValue(t, out node)==false || node == null)
+            {
+				return;
+            }
+
+			m_DLink.RemoveNode(node);
+			m_FindMap.Remove(t);
+		}
+
+		/// <summary>
+		/// 返回结尾节点
+		/// </summary>
+		/// <returns></returns>
+		public T Back() {
+			return m_DLink.Tail == null ? null : m_DLink.Tail.Current;
+		}
+
+		/// <summary>
+		/// 返回节点个数
+		/// </summary>
+		/// <returns></returns>
+		public int Size() {
+			return m_FindMap.Count;
+		}
+
+		/// <summary>
+		/// 判断某个节点是否存在
+		/// </summary>
+		/// <param name="t"></param>
+		/// <returns></returns>
+		public bool Find(T t) {
+			DoubleLinkedListNode<T> node = null;
+            if (m_FindMap.TryGetValue(t,out node)==false || node ==null)
+            {
+				return false;
+            }
+			return true;
+		}
+
+		/// <summary>
+		/// 刷新链表，把指定节点移动到链表头部
+		/// </summary>
+		/// <param name="t"></param>
+		/// <returns></returns>
+		public bool Refresh(T t) {
+			DoubleLinkedListNode<T> node = null;
+            if (m_FindMap.TryGetValue(t,out node)|| node ==null)
+            {
+				return false;
+            }
+
+			m_DLink.MoveToHead(node);
+			return true;
+		}
+	}	
 }
